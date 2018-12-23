6badec198a36fe9ed1822479a7c4437e770d83e9
nwatabou <wataru1.11@gmail.com>
2018-12-24 05:07:52 +0900

    レストラン情報のパースを実装

--- ShareRestaurantApp/Entity/SearchRestaurantResponse.swift
+++ ShareRestaurantApp/Entity/SearchRestaurantResponse.swift
@@ -13,9 +13,40 @@ struct SearchRestaurantResponse: ResponseEntity {
     
     var json: JSON
     
+    struct Restaurant {
+        let id: String
+        let name: String
+        let pr: String?
+        let latitude: Float
+        let longitude: Float
+        let mobileUrl: String
+        let imageUrl: String?
+        let address: String?
+        let tel: String?
+        let openTime: String?
+        let holiday: String?
+    }
+    
+    let restaurants: [Restaurant]
+    
     init(_ json: JSON) {
         self.json = json
         
+        let restaurantArray = json["rest"].arrayValue
+        
+        self.restaurants = restaurantArray.compactMap({ data in
+            return Restaurant(id: data["id"].stringValue,
+                              name: data["name"].stringValue,
+                              pr: data["pr"]["pr_short"].string,
+                              latitude: data["latitude"].floatValue,
+                              longitude: data["longitude"].floatValue,
+                              mobileUrl: data["url_mobile"].stringValue,
+                              imageUrl: data["image_url"].string,
+                              address: data["address"].string,
+                              tel: data["tel"].string,
+                              openTime: data["opentime"].string,
+                              holiday: data["holiday"].string)
+        })
     }
     
 }
